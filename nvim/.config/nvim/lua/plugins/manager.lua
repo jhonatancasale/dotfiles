199bc0cbb0ca6dee48e1203b2d35ac158603039b
jhonatancasale <casale.jhon@gmail.com>
2022-01-08 16:45:15 -0300

    Add completition

--- nvim/.config/nvim/lua/plugins/manager.lua
+++ nvim/.config/nvim/lua/plugins/manager.lua
@@ -40,13 +40,64 @@ packer.init {
 
 -- Install your plugins here
 return packer.startup(function(use)
-  -- My plugins here
+  -- Mandatory-ish
   use "wbthomason/packer.nvim"                  -- Have packer manage itself
   use "nvim-lua/popup.nvim"                     -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim"                   -- Useful lua functions used ny lots of plugins
+  use "kyazdani42/nvim-web-devicons"            -- Provide fancy and useful icons
 
+  -- Colorschemes
   use "gruvbox-community/gruvbox"               -- The de facto default colorscheme
+
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp"                        -- The completion plugin
+  use "hrsh7th/cmp-buffer"                      -- buffer completions
+  use "hrsh7th/cmp-path"                        -- path completions
+  use "hrsh7th/cmp-cmdline"                     -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip"                -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp"                    -- use cmp with LSP
+
+  -- snippets
+  use "L3MON4D3/LuaSnip"                        -- snippet engine
+  use "rafamadriz/friendly-snippets"            -- a bunch of snippets to use
+
+  -- LSP
+  -- use "neovim/nvim-lspconfig"                   -- enable LSP
+  -- use "williamboman/nvim-lsp-installer"         -- simple to use language server installer
+  -- use "tamago324/nlsp-settings.nvim"            -- language server settings defined in json for
+  -- use "jose-elias-alvarez/null-ls.nvim"         -- for formatters and linters
+
+  -- Telescope
+  -- use "nvim-telescope/telescope.nvim"
+  --
+  -- Treesitter
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  }
+
+  -- Utilities
   use "vimwiki/vimwiki"                         -- Wiki language provider for vim
+  use "lewis6991/gitsigns.nvim"                 -- Provide visual clues about git events
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
