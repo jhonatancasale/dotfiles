9d15a6804f7180c4d4d23c47818f86b5123eee51
jhonatancasale <casale.jhon@gmail.com>
2022-06-25 04:26:42 -0300

    Auto-format on save

--- nvim/.config/nvim/lua/plugins/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/lsp/null-ls.lua
@@ -26,15 +26,15 @@ null_ls.setup({
         formatting.rustfmt,
     },
 
-    -- auto forma on save
-    -- on_attach = function(client)
-    --  if client.resolved_capabilities.document_formatting then
-    --      vim.cmd([[
-    --            augroup LspFormatting
-    --                autocmd! * <buffer>
-    --                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-    --            augroup END
-    --            ]])
-    --  end
-    -- end,
+    -- auto format on save
+    on_attach = function(client)
+     if client.resolved_capabilities.document_formatting then
+         vim.cmd([[
+               augroup LspFormatting
+                   autocmd! * <buffer>
+                   autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+               augroup END
+               ]])
+     end
+    end,
 })
