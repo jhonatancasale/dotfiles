4d4cd71d2e8b8aade435c1e68a2b804efcdad5f3
jhonatancasale <casale.jhon@gmail.com>
2022-06-25 04:31:49 -0300

    Update things around ...

--- nvim/.config/nvim/lua/plugins/lsp/handlers.lua
+++ nvim/.config/nvim/lua/plugins/lsp/handlers.lua
@@ -15,7 +15,7 @@ M.setup = function()
 
     local config = {
         -- disable virtual text XXX
-        virtual_text = true,
+        virtual_text = false,
         -- show signs
         signs = {
             active = signs,
@@ -46,18 +46,12 @@ end
 
 local function lsp_highlight_document(client)
     -- Set autocommands conditional on server_capabilities
-    if client.resolved_capabilities.document_highlight then
-        vim.api.nvim_exec(
-            [[
-        augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-        augroup END
-        ]],
-            false
-        )
+    local status_ok, illuminate = pcall(require, "illuminate")
+    if not status_ok then
+        return
     end
+    illuminate.on_attach(client)
+    -- end
 end
 
 local function lsp_keymaps(bufnr)
@@ -85,6 +79,8 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
+    -- vim.notify(client.name .. " starting...")
+    -- TODO: refactor this into a method that checks if string in list
     if client.name == "tsserver" then
         client.resolved_capabilities.document_formatting = false
     end
