eb16b50485e717cf6164d071019831443a4e03ce
casale.jhon@gmail.com <jhonatancasale>
2022-07-02 17:24:53 -0300

    Add tinit to this place

--- zsh/.custom_commands.zsh
+++ zsh/.custom_commands.zsh
@@ -11,20 +11,30 @@ function dorflex() {
   ${WORKSPACE}/virtualenvs/projects/dorflex/dorflex/bin/python ${PROJECTS}/dorflex/dorflex.py $1
 }
 
-function vacations() {
-    _data=$(curl -s -X 'GET' 'http://localhost:8000/vacations' -H 'accept: application/json')
-    if [ $? -ne 0 ]; then
-        echo "Undefined²"
-    else
-        # _date=$(curl -s -X 'GET' 'http://localhost:8000/vacations' -H 'accept: application/json' | python3.10 -c "import sys, json; data = json.load(sys.stdin)['data']; print(data['from'] if len(data) else 'Undefined')")
-        _date=$(echo "${_data}" | python3.10 -c "import sys, json; data = json.load(sys.stdin)['data']; print(data['from'] if len(data) else 'Undefined')")
-        if [ ${_date} = "Undefined" ]; then
-            echo ${_date}
-        else
-            python3.10 -c "from datetime import datetime ; print((datetime.strptime('2021-12-31', '%Y-%m-%d').date() - datetime.today().date()).days)"
-        fi
-    fi
+
+function tinit() {
+    tmux new-session -d -s "gp" -c "${HOME}"
+    tmux new-session -d -s "pythonic" -c "${PROGRAMMING_LANGUAGES}/python"
+    tmux new-session -d -s "rustic" -c "${PROGRAMMING_LANGUAGES}/rust"
+    tmux new-session -d -s "wiki" -c "${WIKIHOME}"
+
+    tmux attach -t gp
 }
+
+# function vacations() {
+#     _data=$(curl -s -X 'GET' 'http://localhost:8000/vacations' -H 'accept: application/json')
+#     if [ $? -ne 0 ]; then
+#         echo "Undefined²"
+#     else
+#         # _date=$(curl -s -X 'GET' 'http://localhost:8000/vacations' -H 'accept: application/json' | python3.10 -c "import sys, json; data = json.load(sys.stdin)['data']; print(data['from'] if len(data) else 'Undefined')")
+#         _date=$(echo "${_data}" | python3.10 -c "import sys, json; data = json.load(sys.stdin)['data']; print(data['from'] if len(data) else 'Undefined')")
+#         if [ ${_date} = "Undefined" ]; then
+#             echo ${_date}
+#         else
+#             python3.10 -c "from datetime import datetime ; print((datetime.strptime('2021-12-31', '%Y-%m-%d').date() - datetime.today().date()).days)"
+#         fi
+#     fi
+# }
 
 function solange() {
   ssh ${SOLANGE_CONN_STRING}
@@ -69,31 +79,34 @@ EOT
 
 function update () {
     _current_path=${PWD}
+
     echo "[Update SO]"
-    sudo apt-get update -y && sudo apt-get upgrade -y
+    # sudo apt-get update -y && sudo apt-get upgrade -y
+    yay -Syu
     echo ""
 
     echo "[Update Rust]"
     rustup update
     echo ""
 
-    echo "[Update zsh]"
-    cd ${ZSH}/themes
-    diff nanotech-jc.zsh-theme nanotech.zsh-theme
-    echo ""
+    # echo "[Update zsh]"
+    # cd ${ZSH}/themes
+    # diff nanotech-jc.zsh-theme nanotech.zsh-theme
+    # echo ""
 
     echo "[Update npm modules]"
     sudo npm update -g
 
-    echo "Attempting to call npm audit fix"
-    cd /tmp
-    npm i --package-lock-only
-    npm audit fix
-    echo ""
+    # echo "Attempting to call npm audit fix"
+    # cd /tmp
+    # npm i --package-lock-only
+    # npm audit fix
+    # echo ""
+
     cd ${_current_path} >/dev/null
 
-    echo "[Update OMZ]"
-    omz update
+    # echo "[Update OMZ]"
+    # omz update
 }
 
 function commit () {
